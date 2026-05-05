4b525ae1c970c7b8be38102ac7ce55d5114c45a8
Jayden Wright <jwright14@byupathway.edu>
2026-05-05 14:13:33 -0600

    added word

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Sandbox Project. And I  have changed");
+        Console.WriteLine("Hello World! This is the Sandbox Project. And I  have changed it");
     }
 }
