c788039405a8790c4b06362a736798b18f2abf42
Jayden Wright <jwright14@byupathway.edu>
2026-06-10 15:28:24 -0600

    Did the activity assignment

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square(5.0, "Blue");
+        Rectangle rectangle = new Rectangle(10.0, 11.0, "Red");
+        Circle circle = new Circle(6.0, "Green");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine(shape.GetColor());
+        }
     }
 }
